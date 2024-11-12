7f4d804232106aa177272ac757f01d753a4333c2
unknown <bsc-06-21@unima.ac.mw>
2024-11-12 18:03:32 +0200

    save

--- Login.tsx
+++ Login.tsx
@@ -3,6 +3,7 @@ import { Text, TextInput, View, TouchableOpacity } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import "./global.css";
 import { useState } from 'react';
 import React from 'react';
@@ -30,6 +31,12 @@ export default function Login({ navigation }) {
   };
 
   const handleLogin = async () => {
+    // Check if both username and password are filled
+    if (!username || !password) {
+      showNotice('Please enter both your registration number and password.');
+      return;
+    }
+
     if (isSubmitting) return;
     setIsSubmitting(true);
 
@@ -40,9 +47,15 @@ export default function Login({ navigation }) {
       });
 
       if (response.status === 200) {
-        localStorage.setItem('token', response.data.token);
+        const { token, userId, registrationNumber, userName } = response.data;
+        
         setLoggedIn(true);
         navigation.navigate('Home'); // Navigate to Home page after successful login
+        
+        // Store token and user details in AsyncStorage
+        await AsyncStorage.setItem('token', token);
+        await AsyncStorage.setItem('userId', userId.toString());
+        await AsyncStorage.setItem('registrationNumber', registrationNumber);
       } else {
         showNotice('Login failed. You entered wrong credentials.');
       }
